c4ec68bd3594695f480de6b32b5df282ba6d5f46
Fletcher <fletcherc89@gmail.com>
2014-02-17 16:43:46 -0800

    cleanup

--- src/extendedhxpunk/ext/EXTJsonSerialization.hx
+++ src/extendedhxpunk/ext/EXTJsonSerialization.hx
@@ -13,23 +13,7 @@ class EXTJsonSerialization
 	public static function encode(o : Dynamic) : String
 	{
 		return TJSON.encode(o, "fancy");
-		
-		//var dynamicToEncode:Dynamic = { };
-		//populate(dynamicToEncode, o);
-		//
-		//return TJSON.encode(dynamicToEncode);
 	}
-	
-	//public static function encodeWithExplicitTypeMapping(objectToEncode:Dynamic, explicitTypes:Map<String, String>):String
-	//{
-		//var fields = Type.getInstanceFields(Type.getClass(objectToEncode));
-		//var dynamicToEncode:Dynamic = { };
-		//
-		//for (field in fields)
-		//{
-			//
-		//}
-	//}
 
 	public static function decode<T>(s : String, typeClass : Class<Dynamic>) : T 
 	{
@@ -101,7 +85,6 @@ class EXTJsonSerialization
 		else
 #end
 		{
-			//var fields = isAnonymous(data) ? Reflect.fields(data) : Type.getInstanceFields(Type.getClass(data));
 			for (field in Reflect.fields(data)) 
 			{
 				if (field == "_explicitType")
@@ -143,14 +126,4 @@ class EXTJsonSerialization
 			}
 		}
 	}
-	
-	//https://groups.google.com/forum/#!msg/haxelang/1ddZUb2XVoU/Y42QaiGPB3kJ
-	//public static inline function isAnonymous(v : Dynamic) : Bool
-	//{
-		//return Reflect.isObject(v) && null == Type.getClass(v);
-	//}
-	//public static function isAnonObj(obj:Dynamic):Bool {
-	  //return Type.typeof(obj) == TObject;
-	//} 
-	//Type.typeof(obj) == TObject && !Std.is(obj, Class) && !Std.is(obj, Enum) 
 }
